3e9dad69e79f214c03b4878b6d4469edd9ce502b
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2017-07-13 16:41:22 -0500

    loading db

--- load_metadata_db.py
+++ load_metadata_db.py
@@ -3,28 +3,31 @@
 from pymongo import MongoClient
 import os
 import unicodecsv
+from bson import json_util
+import xmltodict
+import json
 
-os.chdir('/home/')
+
+filenames_1 = ['AAPB_Metadata_1/'+item for item in os.listdir('AAPB_Metadata_1')if '.pbcore' in item.lower()]
+filenames_2 = ['AAPB_Metadata_2/'+item for item in os.listdir('AAPB_Metadata_2') if '.pbcore' in item.lower()]
+
+pbcore_paths = filenames_1 + filenames_2
+
+print len(pbcore_paths)
+
 
 ## Loading metadata into MongoDB
 mongo_client=MongoClient('mongo', 27017)
-db=mongo_client.pennsound
+db=mongo_client.pbcore_metadata
 
-csvfile = open('/home/PennSound_metadata.csv', 'r')
-reader = unicodecsv.DictReader( csvfile )
+
 
 db.record.drop()
-header= [ 'url','author','title','album','genre','year','comments','track_no','composer','content_group','band','conductor','interpreted_by','location','encoded_by','album_artist','album_type','audio_source_url','commercial_url','copyright_url','encoding_date','internet_radio_url','play_count','publisher','publisher_url','original_release_date','recording_date','release_date','tagging_date','terms_of_use','id3_version','processing_error']
 
-counter=0
 
-for each in reader:
-    try:
-        row={}
-        for field in header:
-            row[field]=each[field]
-        db.record.insert(row)
-        counter+=1
-        #print(counter)
-    except:
-        print("ERROR: "+str(each))
+for xml_path in pbcore_paths:
+    json_text=''
+    json_data = xmltodict.parse(open(xml_path).read())
+    json_text = json.dumps(json_data)
+    data = json_util.loads(json_text)
+    db.record.insert_one(data)
--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 python /home/load_metadata_db.py
 #python /home/app.py
-gunicorn --workers 10 --bind 0.0.0.0:80 wsgi
+#gunicorn --workers 10 --bind 0.0.0.0:80 wsgi
