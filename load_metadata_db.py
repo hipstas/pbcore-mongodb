60a6695a4ff93ad1b51f5707594d77a818b2eece
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2017-07-14 17:06:28 -0500

    running search finally

--- load_metadata_db.py
+++ load_metadata_db.py
@@ -35,19 +35,8 @@ mongo_client=MongoClient('mongo', 27017)
 db=mongo_client.pbcore_metadata
 
 
-db.metadata.drop()
-
-
 import timeit
 tic=timeit.default_timer()
-
-for xml_path in pbcore_paths[200]: #### undo this
-    json_text=''
-    json_data = xmltodict.parse(open(xml_path).read())
-    json_text = json.dumps(json_data['pbcoreDescriptionDocument'])
-    data = json_util.loads(json_text)
-    db.metadata.insert_one(data)
-
 
 
 def merge_dicts(list_of_dicts,item):
@@ -65,69 +54,89 @@ def merge_dicts(list_of_dicts,item):
     for d in list_of_dicts:
         if type(d)==dict:
             keys = [item for item in d]
-            if len(keys)==2:
-                if keys[0][0]=='@':
-                    result.update({d[keys[0]].replace('.','_____'): d[keys[1]]})
-                if keys[1][0]=='@':
-                    result.update({d[keys[1]].replace('.','_____'): d[keys[0]]})
-            else:
-                try:
-                    for key in keys:
-                        if key[0]=="@":
-                            result.update({d[key].replace('.','_____'): d["#text"]})
-                except: print("Guessing classes didn't work for "+str(d))
+            for key in keys:
+                if key[0]=="@":
+                    try:
+                        result.update({d[key].replace('.','<<dot>>'): d["#text"]})
+                    except:
+                        print("Guessing classes didn't work for "+str(d))
         elif (type(d)==str)|(type(d)==unicode):
             result.update({item: d})
+        elif (type(d)==None):
+            foo="great"
         else:
-            print('*')
-            print(str(item)+'.'+str(item)+" is not a dict")
+            print('*&^*')
+            print(str(item)+'.'+str(d)+" is not a dict")
     return result
 
 
+db.metadata.drop()
 
-for xml_path in pbcore_paths: #### undo this
+for xml_path in random.sample(pbcore_paths,2000): #### undo this
     json_text=''
     json_data = xmltodict.parse(open(xml_path).read())
     json_text = json.dumps(json_data['pbcoreDescriptionDocument'])
     data = json_util.loads(json_text)
     try:
+        merged_dicts={}
         for item in data:
-            merged_dicts={}
             if item!=None:
                 if item[0]!='@':
                     #print('---')
                     #print(item)
                     list_of_dicts=data[item]
                     if list_of_dicts != None:
-                        merged_dicts.update({item.replace('.','_____'):merge_dicts(list_of_dicts,item)})
-            merged_dicts.update({"full_text_lower":str(json_text).lower()})
-            temp = json.dumps(merged_dicts)
-            temp = json_util.loads(temp)
-            db.metadata.insert_one(temp)
+                        merged_dicts.update({item.replace('.','<<dot>>'):merge_dicts(list_of_dicts,item)})
+        merged_dicts.update({"full_text_lower":str(json_text).lower()})
+        temp = json.dumps(merged_dicts)
+        temp = json_util.loads(temp)
+        db.metadata.insert_one(temp)
     except Exception as e:
         print(e)
+
+#print(db.metadata.find()[0])
 
 
 print("Completed loading database in "+str(timeit.default_timer() - tic)+" seconds")
 
 
-#db.metadata.count()
-
-
-
-
 ## Searching full text of each metadata
-# wildcard indexing
 
-search_term="AAPB Topical Genre"
-for item in db.metadata.find({ 'pbcoreGenre.@source' :  {'$regex':'.*'+search_term+'.*'}}):
-    print item
+temp_items=[]
+
+search_term='niversity'
+
+search_term=search_term.lower()
+for item in db.metadata.find({ 'pbcoreAnnotation.organization' : {'$regex':'.*'+search_term+'.*'}}):
+    print(item)
+    temp_items.append(item)
+
+len(temp_items)
+
+
+
+
+temp_items=[]
+
+search_term='University'
+
+search_term=search_term.lower()
+for item in db.metadata.find({ "full_text_lower" : {'$regex':'.*'+search_term+'.*'}}):
+    print(item)
+    temp_items.append(item)
+
+len(temp_items)
+
+
+
+
+
 
 
 
 db.metadata.ensure_index(
     [
-        ('pbcoreGenre.@source', 'text')
+        ('pbcoreAnnotation.organization', 'text')
     ],
     name="search_index"
 )
