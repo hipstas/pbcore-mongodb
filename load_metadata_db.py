b90954426aab3f93a7af4caf399a2e139cf395d8
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2017-07-14 11:41:46 -0500

    minor progress

--- load_metadata_db.py
+++ load_metadata_db.py
@@ -7,6 +7,9 @@ from bson import json_util
 import xmltodict
 import json
 
+
+os.chdir('/home/')
+
 try: os.system('wget https://github.com/hipstas/pbcore-mongodb/blob/master/AAPB_Metadata_1.zip?raw=true -O AAPB_Metadata_1.zip')
 except: pass
 
@@ -38,11 +41,40 @@ db.record.drop()
 import timeit
 tic=timeit.default_timer()
 
-for xml_path in pbcore_paths:
+for xml_path in pbcore_paths: #### undo this
     json_text=''
     json_data = xmltodict.parse(open(xml_path).read())
-    json_text = json.dumps(json_data)
+    json_text = json.dumps(json_data['pbcoreDescriptionDocument'])
     data = json_util.loads(json_text)
     db.record.insert_one(data)
 
 print("Completed loading database in "+str(timeit.default_timer() - tic)+" seconds")
+
+
+#db.record.count()
+
+
+
+
+## Searching full text of each record
+# wildcard indexing
+
+search_term="AAPB Topical Genre"
+for item in db.record.find({ 'pbcoreGenre.@source' :  {'$regex':'.*'+search_term+'.*'}}):
+    print item
+
+
+
+db.record.ensure_index(
+    [
+        ('pbcoreGenre.@source', 'text')
+    ],
+    name="search_index"
+)
+
+
+
+
+SEARCH_LIMIT=1000
+query=search_term
+text_results = db.command('text', record, search=query, limit=SEARCH_LIMIT)
